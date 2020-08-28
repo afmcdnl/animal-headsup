581584d9dad9c4d2527b933bf38d9a3e83098087
Andrew <afmcdnl@gmail.com>
2020-08-28 12:26:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.LogoUp, function () {
+    Animal = text_list.removeAt(randint(0, text_list.length))
+    basic.showString("" + (Animal))
+})
+let Animal = ""
+let text_list: string[] = []
+text_list = ["turtle", "frog", "dog", "cat", "snake", "elephant", ""]
+basic.forever(function () {
+    if (0 == text_list.length) {
+        control.reset()
+    }
+})
